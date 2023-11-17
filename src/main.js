22592c7155868a6da77e8607e757f753e0f29534
Jackie Huang <jhuan280@ucsc.edu>
2023-11-17 14:45:09 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Exercise 04: Slime World
+// Name: Jackie Huang
+// Date: 11/17/2023
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
